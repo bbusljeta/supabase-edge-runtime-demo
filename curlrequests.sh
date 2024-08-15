34639df7f108d9fa9bd6911e8607611ce20f7f13
Bojan Bušljeta <bojan@prototyp.digital>
2024-08-15 19:55:35 +0200

    init

---
+++ curlrequests.sh
@@ -0,0 +1,8 @@
+curl -d '{"name":"value1"}' -H "Content-Type: application/json" -X POST http://localhost:8000/hello
+curl -d '{"name":"value2"}' -H "Content-Type: application/json" -X POST http://localhost:8000/hello
+curl -d '{"name":"value3"}' -H "Content-Type: application/json" -X POST http://localhost:8000/hello
+curl -d '{"name":"value4"}' -H "Content-Type: application/json" -X POST http://localhost:8000/hello
+curl -d '{"name":"value5"}' -H "Content-Type: application/json" -X POST http://localhost:8000/hello
+curl -d '{"name":"value6"}' -H "Content-Type: application/json" -X POST http://localhost:8000/hello
+curl -d '{"name":"value7"}' -H "Content-Type: application/json" -X POST http://localhost:8000/hello
+curl -d '{"name":"value8"}' -H "Content-Type: application/json" -X POST http://localhost:8000/hello
---
+++ functions/main/index.ts
@@ -0,0 +1,50 @@
+Deno.serve(async (req: Request) => {
+  const url = new URL(req.url);
+  const { pathname } = url;
+  const path_parts = pathname.split("/");
+  const service_name = path_parts[1];
+
+  if (!service_name || service_name === "") {
+    const error = { msg: "missing function name in request" };
+    return new Response(JSON.stringify(error), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  const servicePath = `/home/deno/functions/${service_name}`;
+  console.error(`serving the request with ${servicePath}`);
+
+  const memoryLimitMb = 150;
+  const workerTimeoutMs = 1 * 60 * 1000;
+  const noModuleCache = false;
+  const importMapPath = null;
+  const envVarsObj = Deno.env.toObject();
+  const envVars = Object.keys(envVarsObj).map((k) => [k, envVarsObj[k]]);
+
+  try {
+    if (pathname === "/metrics") {
+      const metric = await EdgeRuntime.getRuntimeMetrics();
+      return new Response(JSON.stringify(metric), {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    // check https://github.com/supabase/edge-runtime/blob/main/examples/main/index.ts for more details
+    const worker = await EdgeRuntime.userWorkers.create({
+      servicePath,
+      memoryLimitMb,
+      workerTimeoutMs,
+      noModuleCache,
+      importMapPath,
+      envVars,
+    });
+    return await worker.fetch(req);
+  } catch (e) {
+    const error = { msg: e.toString() };
+    return new Response(JSON.stringify(error), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+});
